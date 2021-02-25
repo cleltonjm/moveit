67ad42a53d314720e8a18abfdd5c8192889f9bc0
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2021-02-25 15:36:29 -0300

    Add files via upload

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/cleltonjm.png" alt="Clelton José Mielke"/>
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Clelton José Mielke</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
